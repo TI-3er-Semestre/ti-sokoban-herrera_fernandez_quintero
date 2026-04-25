3b003b09f89d314849c901725a67fb385f74ebfc
Andresmisino <1110289910@u.icesi.edu.co>
2026-04-25 15:24:46 -0500

    test(stack): add CustomStackTest, update GameSnapshot and fix package names

--- src/model/GameSnapshot.java
+++ src/model/GameSnapshot.java
@@ -1,13 +1,15 @@
 package model;
 
-import struture.CustomLinkedList;
+import structure.CustomLinkedList;
 
 public class GameSnapshot {
 
     private Position playerPosition;
     private CustomLinkedList<Position> boxPositions;
+    private int moveCount;
+    private int pushCount;
 
-    public GameSnapshot(Position playerPosition, CustomLinkedList<Box> boxes) {
+    public GameSnapshot(Position playerPosition, CustomLinkedList<Box> boxes, int moveCount, int pushCount) {
         // Copiar la posición del jugador
         this.playerPosition = new Position(playerPosition.getRow(), playerPosition.getColumn());
 
@@ -17,6 +19,8 @@ public class GameSnapshot {
             Box box = boxes.get(i);
             boxPositions.add(new Position(box.getPosition().getRow(), box.getPosition().getColumn()));
         }
+        this.moveCount = moveCount;
+        this.pushCount = pushCount;
     }
 
     public Position getPlayerPosition() {
@@ -26,4 +30,8 @@ public class GameSnapshot {
     public CustomLinkedList<Position> getBoxPositions() {
         return boxPositions;
     }
+
+    public int getMoveCount() { return moveCount; }
+
+    public int getPushCount() { return pushCount; }
 }
