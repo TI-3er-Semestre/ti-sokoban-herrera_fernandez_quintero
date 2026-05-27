ad4426cd9b6edbe00c038d19f7d184f72b0d52f5
Daniel <herreradaniel821@gmail.com>
2026-05-26 21:20:13 -0500

    Agregué tests de Prim: caso normal, peso mínimo y casos borde de 1 y 2 vértices

--- src/test/java/com/icesi/sokoban/structure/GrafoMatrizTest.java
+++ src/test/java/com/icesi/sokoban/structure/GrafoMatrizTest.java
@@ -50,7 +50,7 @@ class GrafoMatrizTest {
     void dfs_grafoNoConexo_produceBosque() {
         grafo.agregarVertice("A");
         grafo.agregarVertice("B");
-        grafo.agregarVertice("C"); // C no está conectado
+        grafo.agregarVertice("C");
         grafo.agregarArista("A", "B", 1);
         assertEquals(3, grafo.dfs("A").size());
     }
@@ -60,5 +60,60 @@ class GrafoMatrizTest {
         grafo.agregarVertice("A");
         grafo.agregarVertice("B");
         assertFalse(grafo.existeArista("A", "B"));
+    }
+
+    @Test
+    void prim_grafoConexo_devuelveArbolDeExpansionMinima() {
+        grafo.agregarVertice("A");
+        grafo.agregarVertice("B");
+        grafo.agregarVertice("C");
+        grafo.agregarVertice("D");
+        grafo.agregarArista("A", "B", 10);
+        grafo.agregarArista("B", "A", 10);
+        grafo.agregarArista("A", "C", 5);
+        grafo.agregarArista("C", "A", 5);
+        grafo.agregarArista("B", "D", 3);
+        grafo.agregarArista("D", "B", 3);
+        grafo.agregarArista("C", "D", 8);
+        grafo.agregarArista("D", "C", 8);
+        CustomLinkedList<int[]> mst = grafo.prim();
+        assertEquals(3, mst.size());
+    }
+
+    @Test
+    void prim_calculaPesoTotalMinimoCorrecto() {
+        grafo.agregarVertice("A");
+        grafo.agregarVertice("B");
+        grafo.agregarVertice("C");
+        grafo.agregarArista("A", "B", 4);
+        grafo.agregarArista("B", "A", 4);
+        grafo.agregarArista("A", "C", 2);
+        grafo.agregarArista("C", "A", 2);
+        grafo.agregarArista("B", "C", 1);
+        grafo.agregarArista("C", "B", 1);
+        CustomLinkedList<int[]> mst = grafo.prim();
+        int pesoTotal = 0;
+        for (int i = 0; i < mst.size(); i++) {
+            pesoTotal += mst.get(i)[2];
+        }
+        assertEquals(3, pesoTotal);
+    }
+
+    @Test
+    void prim_grafoConUnVertice_devuelveListaVacia() {
+        grafo.agregarVertice("A");
+        CustomLinkedList<int[]> mst = grafo.prim();
+        assertEquals(0, mst.size());
+    }
+
+    @Test
+    void prim_grafoConDosVertices_devuelveUnaArista() {
+        grafo.agregarVertice("A");
+        grafo.agregarVertice("B");
+        grafo.agregarArista("A", "B", 7);
+        grafo.agregarArista("B", "A", 7);
+        CustomLinkedList<int[]> mst = grafo.prim();
+        assertEquals(1, mst.size());
+        assertEquals(7, mst.get(0)[2]);
     }
 }
